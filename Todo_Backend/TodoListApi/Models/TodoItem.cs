47f722cd39ad5ac2a280d1e02cd6e21f4b6c02e8
EugeneHohZhaoQuan <hohzq.eugene@gmail.com>
2024-06-15 16:52:12 +0800

    feat: set up backend and db

---
+++ Todo_Backend/TodoListApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+namespace TodoListApi.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Status { get; set; }
+    }
+}
